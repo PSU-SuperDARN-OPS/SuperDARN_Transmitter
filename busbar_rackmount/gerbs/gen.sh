1f8ed6c30ea5f88e1a587a6dc0bda405b17f91fc
Jon Klein <jtklein@alaska.edu>
2015-06-22 13:12:20 -0800

    added rackmount busbar board, updated rackmount busbar board to r1 (fixed powerpole drill sizes)

---
+++ busbar_rackmount/gerbs/gen.sh
@@ -0,0 +1,11 @@
+mv busbar_rackmount-F_Cu.gbr busbar_rackmount.GTL
+mv busbar_rackmount-B_Cu.gbr busbar_rackmount.GBL
+mv busbar_rackmount-B_Mask.gbr busbar_rackmount.GBS
+mv busbar_rackmount-B_SilkS.gbr busbar_rackmount.GBO
+mv busbar_rackmount-Edge_Cuts.gbr busbar_rackmount.GKO
+mv busbar_rackmount-F_Mask.gbr busbar_rackmount.GTS
+mv busbar_rackmount-F_SilkS.gbr busbar_rackmount.GTO
+mv busbar_rackmount.drl busbar_rackmount.xln
+
+rm busbar.zip 
+zip busbar.zip busbar*
